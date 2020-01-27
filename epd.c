4795d6d304ea54d50a67509796e6cc44c91d6ff5
djwj <daniel@wilshirejones.com>
2020-01-27 12:38:38 +0000

    epd: pgm image library for testing, fetch epd info

--- epd.c
+++ epd.c
@@ -1,11 +1,13 @@
+#include<ctype.h>
 #include<fcntl.h>
-#include<unistd.h>
 #include<scsi/scsi_ioctl.h>
 #include<scsi/sg.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<sys/ioctl.h>
+#include<unistd.h>
+#include<./pgm.h>
 
 
 // scsi generic ------------------------------------------------------------- //
@@ -54,34 +56,120 @@ send_message(
 
 // it8951 epd driver -------------------------------------------------------- //
 
+// TODO: epd_info struct is wrong length
+struct epd_info
+{
+  unsigned int standard_cmd_number;
+  unsigned int extend_cmd_number;
+  unsigned int signature;
+  unsigned int version;
+  unsigned int width;
+  unsigned int height;
+  unsigned int update_buffer_address;
+  unsigned int image_buffer_address;
+  unsigned int temperature_segment_number;
+  unsigned int display_modes_count;
+  unsigned int display_mode_frame_counts[8];
+  unsigned int image_buffers_count;
+  unsigned int wbf_sfi_address;
+  unsigned int reserved[9];
+  void *cmd_info_data[1];
+};
+
+enum epd_state
+{ EPD_INIT, EPD_READY, EPD_BUSY };
+
+struct epd
+{
+  int fd;
+  int state;
+  struct epd_info info;
+};
+
+
 int
-init_display(
-  char path[]
+epd_get_system_info(
+  struct epd *display
 )
 {
-  int display_fd = open(path, O_RDWR);
+  if (display->state != EPD_INIT) {
+    return -1;
+  }
+
+  int info_length = sizeof(struct epd_info);
+
+  unsigned char info_command[16] = {
+    0xFE, 0x00, 0x38, 0x39, 0x35, 0x31, 0x80, 0x00, 0x01, 0x00, 0x02, 0x00,
+    0x00, 0x00, 0x00, 0x00
+  };
+
+  int status = send_message(display->fd, 16, &info_command, SG_DXFER_FROM_DEV,
+                            info_length, &(display->info));
+
+  if (status != 0) {
+    return -1;
+  }
+
+  return 0;
+}
+
+
+int
+epd_ensure_it8951_display(
+  struct epd *display
+)
+{
+  if (display->state != EPD_INIT) {
+    return -1;
+  }
 
   unsigned char inquiry_response[40] = { 0 };
   unsigned char inquiry_command[16] =
     { 18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
   int status =
-    send_message(display_fd, 16, &inquiry_command, SG_DXFER_FROM_DEV, 40,
+    send_message(display->fd, 16, &inquiry_command, SG_DXFER_FROM_DEV, 40,
                  &inquiry_response);
 
-  if (status != 0)
-    {
-      return status;
-    }
+  if (status != 0) {
+    return -1;
+  }
 
   char *device_name = malloc(28);
   strncpy(device_name, inquiry_response + 8, 28);
 
-  if (strcmp(device_name, "Generic Storage RamDisc 1.00") == 0)
-    {
-      return 0;
-    }
+  if (strcmp(device_name, "Generic Storage RamDisc 1.00") != 0) {
+    return -1;
+  }
 
-  return -1;
+  return 0;
+}
+
+
+struct epd *
+epd_init(
+  char path[]
+)
+{
+  struct epd *display = (struct epd *) malloc(sizeof(struct epd));
+  memset(display, 0, sizeof(struct epd));
+
+  display->fd = open(path, O_RDWR);
+  display->state = EPD_INIT;
+
+  if (epd_ensure_it8951_display(display) != 0) {
+    free(display);
+    return NULL;
+  }
+
+  epd_get_system_info(display);
+
+  printf("width=%u, height=%u\n", ntohl(display->info.width),
+         ntohl(display->info.height));
+
+  display->state = EPD_READY;
+
+  return display;
 }
 
 
@@ -96,5 +184,14 @@ main(
 )
 {
   printf("Hello, Dan!\n");
-  return init_display("/dev/sg1");
+
+  struct pgm *image = pgm_load("./image.pgm");
+  pgm_print(image);
+
+  /* struct epd *display = epd_init("/dev/sg1"); */
+  /* if (display == NULL) { */
+  /*   return -1; */
+  /* } */
+
+  return 0;
 }
---
+++ pgm.h
@@ -0,0 +1,178 @@
+#include<ctype.h>
+#include<fcntl.h>
+#include<scsi/scsi_ioctl.h>
+#include<scsi/sg.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/ioctl.h>
+#include<unistd.h>
+
+struct pgm
+{
+  int width;
+  int height;
+  int bytes_per_pixel;
+  unsigned char *pixels;
+};
+
+struct pgm *
+pgm_load(
+  char path[]
+)
+{
+  /* Open file for reading */
+  FILE *fd = fopen(path, "r");
+  if (fd == NULL) {
+    printf("Failed to open PGM file.\n");
+    return -1;
+  }
+
+  /* Read in image parameters */
+  char magic_number[3];
+  magic_number[0] = getc(fd);
+  magic_number[1] = getc(fd);
+  magic_number[2] = NULL;
+  if (strcmp(magic_number, "P5\0") != 0) {
+    printf(magic_number);
+    printf("Image at path given is not a PGM file.\n");
+    return -1;
+  }
+
+  char character = getc(fd);
+  while (isspace(character) != 0) {
+    character = getc(fd);
+  }
+
+  int index = 0;
+  char width_str[100] = { 0 };
+  while ((isspace(character) == 0) && (character != EOF)) {
+    width_str[index] = character;
+    character = getc(fd);
+    index++;
+  }
+  width_str[index] = NULL;
+
+  character = getc(fd);
+  while (isspace(character) != 0) {
+    character = getc(fd);
+  }
+
+  index = 0;
+  char height_str[100] = { 0 };
+  while ((isspace(character) == 0) && (character != EOF)) {
+    height_str[index] = character;
+    character = getc(fd);
+    index++;
+  }
+  height_str[index] = NULL;
+
+  character = getc(fd);
+  while (isspace(character) != 0) {
+    character = getc(fd);
+  }
+
+  index = 0;
+  char max_gray_str[100] = { 0 };
+  while ((isspace(character) == 0) && (character != EOF)) {
+    max_gray_str[index] = character;
+    character = getc(fd);
+    index++;
+  }
+  max_gray_str[index] = NULL;
+
+  int width = atoi(width_str);
+  int height = atoi(height_str);
+  int max_gray = atoi(max_gray_str);
+
+  int bytes_per_pixel;
+  if (max_gray < 256) {
+    bytes_per_pixel = 1;
+  } else {
+    bytes_per_pixel = 2;
+  }
+
+  printf("Loading PGM:\n");
+  printf("\twidth=%d, height=%d, max_gray=%d, bytes_per_pixel=%d\n", width,
+         height, max_gray, bytes_per_pixel);
+
+  /* Allocate appropriately sized memory */
+  int num_bytes = width * height * bytes_per_pixel;
+  unsigned char *bytes = malloc(sizeof(unsigned char) * num_bytes);
+
+  while (isspace(character) != 0) {
+    character = getc(fd);
+  }
+
+  bytes[0] = character;
+  for (index = 1; index < num_bytes; index++) {
+    character = getc(fd);
+    bytes[index] = (unsigned char) character;
+  }
+
+  struct pgm *image = malloc(sizeof(struct pgm));
+  image->width = width;
+  image->height = height;
+  image->bytes_per_pixel = bytes_per_pixel;
+  image->pixels = bytes;
+
+  fclose(fd);
+  return image;
+}
+
+int
+pgm_print(
+  struct pgm *image
+)
+{
+  char greyscale[] = " .:-=+*#%@";
+
+  int x, y;
+  for (x = 0; x < image->width; x++) {
+    for (y = 0; y < image->height; y++) {
+      if (image->pixels[x + y * image->width] < 30) {
+        putchar(greyscale[9]);
+        continue;
+      }
+      if (image->pixels[x + y * image->width] < 60) {
+        putchar(greyscale[8]);
+        continue;
+      }
+      if (image->pixels[x + y * image->width] < 90) {
+        putchar(greyscale[7]);
+        continue;
+      }
+      if (image->pixels[x + y * image->width] < 120) {
+        putchar(greyscale[6]);
+        continue;
+      }
+      if (image->pixels[x + y * image->width] < 150) {
+        putchar(greyscale[5]);
+        continue;
+      }
+      if (image->pixels[x + y * image->width] < 180) {
+        putchar(greyscale[4]);
+        continue;
+      }
+      if (image->pixels[x + y * image->width] < 210) {
+        putchar(greyscale[3]);
+        continue;
+      }
+      if (image->pixels[x + y * image->width] < 240) {
+        putchar(greyscale[2]);
+        continue;
+      }
+      if (image->pixels[x + y * image->width] < 255) {
+        putchar(greyscale[1]);
+        continue;
+      }
+      if (image->pixels[x + y * image->width] == 255) {
+        putchar(greyscale[0]);
+        continue;
+      }
+    }
+    printf("\n");
+  }
+
+  return 0;
+}

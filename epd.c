50b4dbeac840eb23b49c4d82da210b119076965a
djwj <daniel@wilshirejones.com>
2020-07-25 14:19:14 +0100

    translating python - checkpoint
    haven't run it in a while so this commit is probably buggy. waiting
    until i have enough to draw images on the display before i debug.

--- epd.c
+++ epd.c
@@ -10,8 +10,40 @@
 #include<./pgm.h>
 #include<./epd.h>
 
+/* SCSI Generic ----------------------------------------------------------------
 
-// scsi generic ------------------------------------------------------------- //
+A bunch of tools and utilities for playing with SCSI generic
+devices. Most of the stuff we need is in stuff is in scsi/sg.h so this
+should be quite minimal.
+
+#+begin_quote
+    SCSI generic driver supports many typical system calls for
+    character device, such as open(), close(), read(), write, poll(),
+    ioctl(). The procedure for sending SCSI commands to a specific
+    SCSI device is also very simple:
+
+    1. Open the SCSI generic device file (such as sg1) to get the file
+        descriptor of SCSI device.
+    2. Prepare the SCSI command.
+    3. Set related memory buffers.
+    4. Call the ioctl() function to execute the SCSI command.
+    5. Close the device file.
+#+end_quote
+
+A good, if not verbose, reference for this is the actual Linux source
+code. Here is the file & commit this library was written against:
+    https://github.com/torvalds/linux/blob/6f0d349d922ba44e4348a17a78ea51b7135965b1/include/scsi/sg.h#L44
+
+*/
+
+// SCSI operation codes (used as the first byte of an CommandDescriptorBlock).
+enum sg_opcode
+{
+  SG_OP_INQUIRY = 0x12,
+  SG_OP_CUSTOM = 0xFE
+};
+
+
 int
 send_message(
   int fd,
@@ -56,6 +88,67 @@ send_message(
 
 
 // it8951 epd driver -------------------------------------------------------- //
+enum epd_load_image_args_address_mode
+{
+
+};
+
+enum epd_update_mode
+{
+  // Clear Mode:
+  EPD_UPD_RESET = 0,
+
+  // 8 bits per pixel:
+  EPD_UPD_EIGHT_BIT_SLOW = 2,
+  EPD_UPD_EIGHT_BIT_MEDIUM = 3,
+
+  // Does full update of area requested but only
+  // updates the non-white values.
+  //  - Maybe not actually sure. Seems more
+  //    complicated.
+  EPD_UPD_EIGHT_BIT_FAST = 4,
+  EPD_UPD_TWO_BIT = 5,
+
+  // Below values require < 8 bits per pixel.
+  EPD_UPD_ONE_BIT = 1,          // Binary
+
+  // I think the following is good for 1-bit data
+  // but only works with 1-bit data (e.g. if you
+  // give it grey values it will do nothing with
+  // them).
+  //   - No, not quite.
+  EPD_UPD_ONE_BIT_FAST = 6,     // Fast Binary?
+
+  // Seems to do thresholding of some sort (maybe
+  // 2 or 3 bits?)
+  // - Does 7 apply a binary mask to your next update? No
+  // - Supports two bits per pixel I believe
+  // - Does it do an auto diff with the image buffer?
+  EPD_UPD_TWO_OR_FOUR_BITS = 7, // 2-bits per pixel
+};
+
+// Fill this code to CDB[6].
+enum epd_opcode
+{
+  EPD_OP_GET_SYS = 0x80,
+  EPD_OP_READ_MEM = 0x81,
+  EPD_OP_WRITE_MEM = 0x82,
+  EPD_OP_DPY_AREA = 0x94,
+  EPD_OP_LD_IMG_AREA = 0xA2,
+  EPD_OP_PMIC_CTRL = 0xA3,
+  EPD_OP_FAST_WRITE_MEM = 0xA5,
+  EPD_OP_AUTO_RESET = 0xA7,
+
+  // Following are not listed in the docs, but found in the example
+  // code. Danger?
+  EPD_OP_USB_SPI_ERASE = 0x96,
+  EPD_OP_USB_SPI_READ = 0x97,
+  EPD_OP_USB_SPI_WRITE = 0x98,
+  EPD_OP_IMG_CPY = 0xA4,        // Not used in Current Version (IT8961 Samp only)
+  EPD_OP_READ_REG = 0x83,
+  EPD_OP_WRITE_REG = 0x84,
+  EPD_OP_FSET_TEMP = 0xA4,
+};
 
 // TODO: epd_info struct is wrong length
 struct epd_info
@@ -77,15 +170,130 @@ struct epd_info
   void *cmd_info_data[1];
 };
 
+
 enum epd_state
 { EPD_INIT, EPD_READY, EPD_BUSY };
+
 
 struct epd
 {
   int fd;
   int state;
+  unsigned int max_transfer;
   struct epd_info info;
 };
+
+int
+epd_transfer_image_chunk(
+  struct epd *display,
+  unsigned int base_x,
+  unsigned int base_y,
+  unsigned int chunk_x,
+  unsigned int chunk_y,
+  unsigned int chunk_width,
+  unsigned int chunk_height,
+  struct pgm image,
+)
+{
+}
+
+
+int
+epd_transfer_image(
+  struct epd *display,
+  unsigned int x,
+  unsigned int y,
+  struct pgm image,
+)
+{
+
+  // Transfer the image given in `image` into the image buffer in
+  // `epd`s memory, at the offset determined by `x` and `y`.
+
+  // Chunked based on the max transfer size of the display, taking
+  // whole rows of the input image (under the assumption that
+  // image->width < epd->max_tranfer).
+
+  unsigned int max_chunk_height = epd->max_transfer / image->width;
+
+  unsigned int start_row, end_row;
+  unsigned int start_x, start_y;
+  unsigned int chunk_width, chunk_height;
+  unsigned int chunk_address;
+
+  for (start_row = 0; start_row < image->height;
+       start_row += max_chunk_height) {
+
+    end_row = start_row + max_chunk_height;
+    if (end_row > image->height) {
+      end_row = image->height;
+    }
+
+    chunk_width = image->width;
+    chunk_height = end_row - start_row;
+
+    chunk_address_in_src_image = 0 + start_row * chunk_width;
+    chunk_address_in_epd_image = x + (y + start_row) * epd->info->width;
+    chunk_address_in_epd_memory =
+      epd->info->image_buffer_address + chunk_address_in_epd_image;
+
+    unsigned char load_image_command[16] = {
+      SG_OP_CUSTOM, 0, 0, 0, 0, 0,
+      EPD_OP_LD_IMG_AREA, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    };
+
+    unsigned char load_image_command[16] = {
+      SG_OP_CUSTOM, 0, 0, 0, 0, 0,
+      EPD_OP_LD_IMG_AREA, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    };
+  }
+}
+
+
+int
+epd_draw(
+  struct epd *display,
+  unsigned int x,
+  unsigned int y,
+  struct pgm image,
+  struct epd_update_mode update_mode,
+)
+{
+  if (display->state != EPD_READY) {
+    return -1;
+  }
+
+  if (image->bytes_per_pixel != 1) {
+    printf("epd_draw: only supports images with 1 byte per pixel\n");
+    return -1;
+  }
+
+  if (image->x + image->width > epd->info->width
+      || image->y + image->height > epd->info->height) {
+    printf("epd_draw: cannot draw image outside of display boundary\n");
+    return -1;
+  }
+
+  if (image->x == 0 && image->y == 0 && image->width == epd->info->width
+      && image->height == epd->info->height) {
+    printf("epd_draw: detected full image update\n");
+    // TODO: Can we optimise this case? I believe there are special ops we can do.
+  }
+
+  unsigned char draw_command[16] = {
+    SG_OP_CUSTOM, 0, 0, 0, 0, 0, EPD_OP_DPY_AREA, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  };
+
+  int status = send_message(display->fd, 16, &draw_command, SG_DXFER_TO_DEV,
+                            info_length, &(display->info));
+
+  if (status != 0) {
+    return -1;
+  }
+
+  return 0;
+
+}
 
 
 int
@@ -100,8 +308,8 @@ epd_get_system_info(
   int info_length = sizeof(struct epd_info);
 
   unsigned char info_command[16] = {
-    0xFE, 0x00, 0x38, 0x39, 0x35, 0x31, 0x80, 0x00, 0x01, 0x00, 0x02, 0x00,
-    0x00, 0x00, 0x00, 0x00
+    SG_OP_CUSTOM, 0, 0x38, 0x39, 0x35, 0x31, EPD_OP_GET_SYS, 0, 0x01, 0, 0x02,
+    0, 0, 0, 0, 0
   };
 
   int status = send_message(display->fd, 16, &info_command, SG_DXFER_FROM_DEV,
@@ -157,6 +365,7 @@ epd_init(
 
   display->fd = open(path, O_RDWR);
   display->state = EPD_INIT;
+  display->max_transfer = 60000;
 
   if (epd_ensure_it8951_display(display) != 0) {
     free(display);
